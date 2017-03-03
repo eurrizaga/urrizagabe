388d42f90b3f27e88c84fe5df709eb8f929349f8
eurrizaga <eurrizaga@gmail.com>
2017-03-02 21:29:31 -0300

    x

--- controllers/general.php
+++ controllers/general.php
@@ -1,16 +1,16 @@
 <?php
 function get($db, $query){
 	$myArray = array();
-    if ($result = $db->query($query)) {
-      $tempArray = array();
-      while($row = $result->fetch_object()) {
-        $tempArray = $row;
-        array_push($myArray, $tempArray);
-      }
-      $result = json_encode($myArray);
+  if ($result = $db->query($query)) {
+    $tempArray = array();
+    while($row = $result->fetch_object()) {
+      $tempArray = $row;
+      array_push($myArray, $tempArray);
     }
-    else{
-      $result = $query;
-    }
-    
+    $result = json_encode($myArray);
+  }
+  else{
+    $result = $query;
+  }
+  return $result;
 }
